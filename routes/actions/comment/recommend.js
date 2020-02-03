3647fb035f3d73c74b26c00c663fda1a8cad5f53
BM_laoli <861795660@qq.com>
2020-02-03 16:44:48 +0800

    初步实现的全栈项目从后台到前台数据渲染

---
+++ routes/actions/comment/recommend.js
@@ -0,0 +1,7 @@
+const { Comment } = require('../../../model/Comment');
+
+module.exports = async(req, res) => {
+    const posts = await Comment.find({ state: 1 }).populate('author', '-password').sort('-createAt').limit(5)
+        // 响应
+    res.send(posts);
+}
